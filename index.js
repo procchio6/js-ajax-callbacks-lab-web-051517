1786b0c2e9bb812c0c0405693bae4c0f2e01a56a
Pat Rocchio <procchio6@gmail.com>
2017-06-30 14:02:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,54 @@
 $(document).ready(function (){
+  $('#searchButton').on('click', searchRepositories)
+  $('#results').on('click', '.commitLink', showCommits)
 });
+
+function searchRepositories(event) {
+  event.preventDefault()
+  console.log('Search button pressed')
+  let searchTerm = $('#searchTerms').val()
+  $.get("https://api.github.com/search/repositories?q="+searchTerm, displayRepositories)
+    .fail(displayError)
+}
+
+function displayRepositories(data) {
+  $results = $('#results')
+  $results.empty()
+  data.items.forEach(item => {
+    $results.append(renderRepo(item))
+  })
+}
+
+function renderRepo(repo) {
+  return `<a href="${repo.html_url}">${repo.name}</a> <br>` +
+    `${repo.description} <br>` +
+    `<img class="avatar" src="${repo.owner.avatar_url}"/> <br>` +
+    `<a href="${repo.owner.html_url}">${repo.owner.login}</a> <br>` +
+    `<a class="commitLink" href="" rel="${repo.commits_url}">Show Commits</a>` +
+    `<br><br>`
+}
+
+function showCommits(event) {
+  event.preventDefault()
+  let commitUrl = $(this).attr('rel').replace(/({.+})/,"")
+  $.get(commitUrl, displayCommits).fail(displayError)
+}
+
+function displayCommits(commits) {
+  let $details = $('#details')
+  $details.empty()
+  commits.forEach(commit => {
+    $details.append(renderCommit(commit))
+  })
+}
+
+function renderCommit(commit) {
+  let commitHTML = `${commit.sha} <br> ${commit.author.login} <br>` +
+    `<img class="avatar" src="${commit.author.avatar_url}" /> <br>`
+  return commitHTML
+}
+
+function displayError(error) {
+  console.log("Ooops...something went wrong!")
+  $('#errors').append("I'm sorry, there's been an error. Please try again.")
+}
